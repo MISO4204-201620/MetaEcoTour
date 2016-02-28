94ee954cb19349da5a1dc2609421985cf5b7e3ed
Jose Luis Pulgarin <jl.pulgarin@uniandes.edu.co>
2016-02-27 22:54:45 -0500

    Entidad itemServicio

---
+++ app/models/catalogo/ItemServicio.java
@@ -0,0 +1,45 @@
+package models.catalogo;
+
+import javax.persistence.*;
+
+/**
+ * Created by JoséLuis on 27/02/2016.
+ */
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "ItemServicio.findByPaquete", query = "SELECT is FROM ItemServicio is WHERE is.idPaquete = :idPaquete"),
+        @NamedQuery(name = "ItemServicio.findByServicio", query = "SELECT is FROM ItemServicio is WHERE is.idServicio = :idServicio ")
+})
+public class ItemServicio {
+
+    @Id @Column(nullable=false)
+    private Long idPaquete;
+    @Id @Column(nullable=false)
+    private Long idServicio;
+    @Column(nullable=false)
+    private int cantidad;
+
+    public Long getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(Long idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+    public Long getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(Long idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
