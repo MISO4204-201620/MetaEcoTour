c1736237f35af41aa16af9114754edc4623d2a35
Jorge Rubiano <ostricajh@gmail.com>
2016-03-09 19:01:24 -0500

    Front - Login/Catálogo
    Se realiza el consumo de los servicio del Back de Login y catálogo

--- public/js/script_catalogo.js
+++ public/js/script_catalogo.js
@@ -1,43 +1,42 @@
 $(function()
 {
+    //Reemplazar por la url de Amazon...
+    var UrlImagen = "http://localhost:8887/maestria/base/img/portada/";
     var baseItem = function(data)
     {
         //debugger;
+        var imagen = UrlImagen + "/" + data.imagen;
         var txt = '<div class="col-sm-4 col-lg-4 col-md-4">' +
                   '<div class="thumbnail">' +
-                  '<img src="img/portada/'+(data.front)+'" alt="">' +
+                  '<img src = "'+(imagen)+'" alt="">' +
                   '<div class="caption">' +
-                  '<h4 class="pull-right">$'+(data.price)+'</h4>' +
-                  '<h4><a href="#">'+(data.name)+'</a>' +
+                  '<h4 class="pull-right">$'+(data.precioActual)+'</h4>' +
+                  '<h4><a href="#">'+(data.nombre)+'</a>' +
                   '</h4>' +
-                  '<p>'+(data.summary)+'</p>' +
+                  '<p>'+(data.descripcion)+'</p>' +
                   '</div>' +
                   '<div class="ratings">' +
-                  '<p class="pull-right">'+(data.reviews)+'</p>' +
+                  '<p class="pull-right">'+(data.puntuacion)+'</p>' +
                   '<p>';
         //Para las estrellas...
         for(var i = 1; i <= 5; i++)
         {
-            txt += '<span class = "glyphicon ' + (i <= data.rating ? 'glyphicon-star' : 'glyphicon-star-empty' ) + '"></span>';
+            txt += '<span class = "glyphicon ' + (i <= data.puntuacion ? 'glyphicon-star' : 'glyphicon-star-empty' ) + '"></span>';
             //console.log(i);
         }
         txt += '</p></div></div></div>';
         return txt;
     };
 
-    //Para activar el activator:
-    //export PATH=/Users/jorgerubiano/Documents/activator/:$PATH
     var elementos = (function elementos()
     {
         $.getJSON( "/api/productos/SER", function(data)
         {
             console.log(data);
-            /*
             data.forEach(function(item)
             {
                 $("#catalog").append(baseItem(item));
             });
-            */
         });
         return elementos;
     })();
--- public/js/script_login.js
+++ public/js/script_login.js
@@ -1,4 +1,29 @@
 /**
  * Created by jorgerubiano on 6/03/16.
  */
-//script_login
+$(function()
+{
+    $( "#form" ).submit(function( event )
+    {
+        var dataUser = {
+                            correo : $("#inputEmail").val(),
+                            clave : $("#inputPassword").val()
+        };
+        $.ajax(
+        {
+            url 		: "/login",
+            type 		: "POST",
+            data 		: JSON.stringify(dataUser),
+            dataType 	: "json",
+            contentType: "application/json; charset=utf-8"
+        }).done(function(data)
+        {
+            console.log(data);
+            window.location.href = "/catalog"
+        }).error(function(request, status, error)
+        {
+            sweetAlert("Error", "No ha sido posible realizar la autenticación!", "error");
+        });
+        event.preventDefault();
+    });
+});
