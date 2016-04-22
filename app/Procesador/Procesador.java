1181ab7d538c3d51aa4dfe61b9e3239c29039074
crimago <manuel.mancipe@gmail.com>
2016-04-22 16:28:16 -0500

    Se agrega la clase encargada del procesador, ademas se edita la clase test y el arreglo a constraint de la clase usuario

---
+++ app/Procesador/Procesador.java
@@ -0,0 +1,15 @@
+package Procesador;
+
+/**
+ * Created by manuel on 22/04/16.
+ */
+public class Procesador {
+
+
+
+    public static void main (String [ ] args) {
+
+        //Aquí las instrucciones del método
+        System.out.println("prueba de ejecucion del procesador");
+    }
+}
