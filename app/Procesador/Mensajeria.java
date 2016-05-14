180e9359dd554d52086ae24de01634104a75b4fb
crimago <manuel.mancipe@gmail.com>
2016-05-14 14:00:26 -0500

    Manejo de variabilidad de caracteristica de mensajeria con anotaciones

---
+++ app/Procesador/Mensajeria.java
@@ -0,0 +1,16 @@
+package Procesador;
+
+
+import play.mvc.With;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by crimago on 10/05/16.
+ */
+@With(MensajeriaAction.class)
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Mensajeria {
+    boolean value() default true;
+}
---
+++ app/Procesador/MensajeriaAction.java
@@ -0,0 +1,70 @@
+package Procesador;
+
+import controllers.contratos.mensajerias.IComentario;
+import controllers.contratos.usuarios.IUsuarios;
+import controllers.implementacion.mensajerias.Comentarios;
+import controllers.implementacion.usuarios.Usuarios;
+import models.mensajeria.Comentario;
+import models.mensajeria.MensajeDTO;
+import models.usuario.UsuarioDTO;
+import play.Logger;
+import play.libs.F;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.util.List;
+
+/**
+ * Created by crimago on 13/05/16.
+ */
+public class MensajeriaAction extends Action<Mensajeria> {
+
+    private static IComentario comentarios = new Comentarios();
+    private static IUsuarios usuarios = new Usuarios();
+    @Override
+    @play.db.jpa.Transactional
+    public F.Promise<Result> call(Http.Context ctx) throws Throwable {
+        Logger.info("Calling action for {}", ctx);
+
+        List<MensajeDTO> mensajesUsuario = null;
+        List<UsuarioDTO> usuarioDTOs = null;
+        String[] paths = ctx.request().path().split("/");
+        String primero = "";
+        String segundo = "";
+        if (paths.length > 5){
+            primero = paths[4];
+            segundo = paths[5];
+        }
+
+        String metodo = (String)ctx.args.get("ROUTE_ACTION_METHOD");
+
+        if (configuration.value()){
+
+            if ("getMensajesByUsuarios".equals(metodo)){
+                long segundoL =  Long.valueOf(segundo);
+                long primeroL =  Long.valueOf(primero);
+                mensajesUsuario = comentarios.getMensajesByUsuarios(primeroL, segundoL);
+                ctx.args.put("mensajesUsuario", mensajesUsuario);
+
+            } else {
+                long primeroL =  Long.valueOf(primero);
+                Comentario.Tipo tipo = Comentario.Tipo.MENSAJE;
+                if (segundo.equals(Comentario.Tipo.COMENTARIO.name()))
+                {
+                    tipo = Comentario.Tipo.COMENTARIO;
+                }
+                if (segundo.equals(Comentario.Tipo.PREGUNTA.name()))
+                {
+                    tipo = Comentario.Tipo.PREGUNTA;
+                }
+                usuarioDTOs = usuarios.getUsuariosInteraccionMensajes(primeroL, tipo);
+                ctx.args.put("usuarioDTOs", usuarioDTOs);
+
+            }
+        }
+
+        return delegate.call(ctx);
+    }
+}
+
